ae9665bac41ce34aff709c76e1b7d007064c1e1f
Shayxnazar <116631109+komilovshayxnazar@users.noreply.github.com>
2025-12-18 15:36:24 +0500

    Add files via upload

---
+++ 90. Subsets II.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        def backtrack(start, path):
+            # Append a copy of current subset
+            res.append(path[:])
+
+            for i in range(start, len(nums)):
+                # Skip duplicates at this level
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                # Include nums[i]
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
